faf8061ee54ddfb9da04238fcbab153c9e5d50fe
samiksha252 <samikshaaswale02@gmail.com>
2024-06-20 23:31:40 +0530

    PQR_package

---
+++ pqr_25/src/pqr_25/PQR.java
@@ -0,0 +1,15 @@
+package pqr_25;
+
+public class PQR {
+	
+	public void add() {
+		System.out.println("add number");
+	}
+
+	public static void main(String[] args) {
+		PQR ok = new PQR();
+		ok.add();
+
+	}
+
+}
